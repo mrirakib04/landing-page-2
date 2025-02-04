f19d896b347f894463184a81c37f88ffcfc5f0ac
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-02-04 21:08:41 +0600

    why web development section added

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,10 +2,13 @@
 import React from "react";
 import BannnerImg from "./assets/img/Hero-bg.png";
 import BannnerHero from "./assets/img/Group.png";
+import groupImg1 from "./assets/img/Layer_1.png";
+import groupImg2 from "./assets/img/Group 3.png";
+import groupImg3 from "./assets/img/Group 4.png";
 
 const Home = () => {
   return (
-    <div className="flex flex-col max-w-[1480px] mx-auto px-2">
+    <div className="flex flex-col items-center max-w-[1480px] mx-auto px-2">
       {/* header nav */}
       <nav className="w-full container mx-auto mt-5">
         <ul className="w-full flex items-center justify-between">
@@ -40,6 +43,71 @@ const Home = () => {
         <button className="text-lg font-medium text-white bg-black py-2 px-4 rounded-lg transition hover:bg-gray-700 mt-5 cursor-pointer">
           Contact With Me
         </button>
+      </div>
+      {/* why web development section */}
+      <div className="px-2 mt-10 w-full">
+        <div className="container mx-auto rounded-lg max-w-6xl bg-[#FDCCC5]/30 p-5 sm:py-10 flex flex-col items-center gap-5">
+          <div className="mb-5 text-center">
+            <h3 className="md:text-4xl sm:text-3xl text-xl font-bold text-center">
+              Why Web Development??
+            </h3>
+            <p className="sm:text-lg text-base mt-2">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
+              quaerat dicta delectus, consequatur assumenda quos.
+            </p>
+          </div>
+          <div className="flex sm:flex-row flex-col items-center sm:flex-nowrap flex-wrap gap-5 p-5 rounded-lg bg-gradient-to-b sm:bg-gradient-to-r from-white via-white to-[#FDCCC5]/5">
+            <div className="sm:w-[70%]">
+              <h4 className="text-lg sm:text-xl md:text-2xl font-semibold mb-3">
+                I will do Coding and Change The World
+              </h4>
+              <p className="md:text-lg text-sm mb-2">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et
+                excepturi dolor suscipit quae tenetur omnis.
+              </p>
+              <button className="py-1 md:text-lg text-sm px-4 border-2 rounded-md border-black/70">
+                Learn More
+              </button>
+            </div>
+            <div className="sm:w-[30%]">
+              <img src={groupImg1} alt="" className="sm:w-full max-w-40" />
+            </div>
+          </div>
+          <div className="flex sm:flex-row flex-col items-center sm:flex-nowrap flex-wrap gap-5 p-5 rounded-lg bg-gradient-to-b sm:bg-gradient-to-r from-[#FDCCC5]/5 via-white to-white">
+            <div className="sm:w-[30%]">
+              <img src={groupImg2} alt="" className="sm:w-full max-w-40" />
+            </div>
+            <div className="sm:w-[70%]">
+              <h4 className="text-lg sm:text-xl md:text-2xl font-semibold mb-3">
+                I will help People by creating Modern Web System
+              </h4>
+              <p className="md:text-lg text-sm mb-2">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et
+                excepturi dolor suscipit quae tenetur omnis.
+              </p>
+              <button className="py-1 md:text-lg text-sm px-4 border-2 rounded-md border-black/70">
+                Learn More
+              </button>
+            </div>
+          </div>
+          <div className="flex sm:flex-row flex-col items-center sm:flex-nowrap flex-wrap gap-5 p-5 rounded-lg bg-gradient-to-b sm:bg-gradient-to-r from-white via-white to-[#FDCCC5]/5">
+            <div className="sm:w-[70%]">
+              <h4 className="text-lg sm:text-xl md:text-2xl font-semibold mb-3">
+                I will do Coding as My Profession
+              </h4>
+              <p className="md:text-lg text-sm mb-2">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et
+                excepturi dolor suscipit quae tenetur omnis.
+              </p>
+              <button className="py-1 md:text-lg text-sm px-4 border-2 rounded-md border-black/70">
+                Learn More
+              </button>
+            </div>
+            <div className="sm:w-[30%]">
+              <img src={groupImg3} alt="" className="sm:w-full max-w-40" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
